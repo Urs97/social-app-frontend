bda434f01a01a3ac0c1fe7913c0e5e384bb9304b
Urs97 <64044473+Urs97@users.noreply.github.com>
2021-04-10 05:49:49 +0200

    added prettier.yml config file

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -9,16 +9,16 @@ const Post = ({ post }) => {
     
     return (
         <section>
-        <h1>{post.title}</h1>
-        <h3>{post.author}</h3>
-        <h3>{date}</h3>
-        <p>{post.content}</p>
-        <span>{post.likeCount}</span>
-        <span>{post.dislikeCount}</span>
-        <button onClick={() => dispatch(likePost(post._id))}>Like</button>
-        <button onClick={() => dispatch(dislikePost(post._id))}>Dislike</button>
-        <button onClick={() => dispatch(deletePost(post._id))}>Delete</button>
-        <button onClick={() => dispatch(setCurrentPost(post._id))}>Edit</button>
+            <h1>{post.title}</h1>
+            <h3>{post.author}</h3>
+            <h3>{date}</h3>
+            <p>{post.content}</p>
+            <span>{post.likeCount}</span>
+            <span>{post.dislikeCount}</span>
+            <button onClick={() => dispatch(likePost(post._id))}>Like</button>
+            <button onClick={() => dispatch(dislikePost(post._id))}>Dislike</button>
+            <button onClick={() => dispatch(deletePost(post._id))}>Delete</button>
+            <button onClick={() => dispatch(setCurrentPost(post._id))}>Edit</button>
         </section>
     )
 }
